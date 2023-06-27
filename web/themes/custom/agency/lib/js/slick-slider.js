d143e2148d8255aa03353bd7c630cdc51bfb7569
Poonam Gaigole <poonam.gaigole@gmail.com>
2023-06-27 16:44:47 +0530

    Made slick slider responsive and modified ranges of tablets

--- web/themes/custom/agency/lib/js/slick-slider.js
+++ web/themes/custom/agency/lib/js/slick-slider.js
@@ -1,19 +1,33 @@
 (function ($) {
     Drupal.behaviors.slick_slider = {
         attach: function (context, settings) {
-            console.log('Hi poonam');
-            console.log(context);
             $('.slick-slider').slick({
                 prevArrow: '<div class="slick-prev"><i class="fa fa-circle-chevron-left fa-xl" aria-hidden="true"></i></div>',
                 nextArrow: '<div class="slick-next"><i class="fa fa-circle-chevron-right fa-xl" aria-hidden="true"></i></div>',
                 dots: false,
                 infinite: false,
                 speed: 300,
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 2,
                 centerMode: false,
                 variableWidth: true,
-                arrows: true
+                arrows: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                        }
+                    }
+                ]
             });
         }
     }
